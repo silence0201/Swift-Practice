5b8798af9a5a4dd89a5008c250f9d048b33ae5f6
Silence <374619540@qq.com>
2017-03-30 00:22:29 +0800

    WaveView

--- WaveView/WaveView/ViewController.swift
+++ WaveView/WaveView/ViewController.swift
@@ -10,16 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let wave = WaveView(frame: CGRect(x: 0.0, y: 200.0, width: SWidth, height: 31))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func setupView() {
+        wave.waveSpeed = 10
+        wave.angularSpeed = 1.5
+        view.addSubview(wave)
+        
+        let whiteView = UIView(frame: CGRect(x: 0.0, y: 230, width: SWidth, height: SHeight-230))
+        whiteView.backgroundColor = .white
+        view.addSubview(whiteView)
+        
+        let btn = UIButton(frame: CGRect(x: 100, y: 400, width: SWidth-200, height: 30))
+        btn.setTitle("开始", for: .normal)
+        btn.setTitle("结束", for: .selected)
+        btn.setTitleColor(.orange, for: .normal)
+        view.addSubview(btn)
+        btn.addTarget(self, action: #selector(changeStatus(_:)), for: .touchUpInside)
     }
-
+    
+    @objc private func changeStatus(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        sender.isSelected ? wave.startWave() : wave.stopWaver()
+    }
 
 }
 
