4616e70ddf01f3045e8e342e1b1c687e3b5cd37e
Silence <374619540@qq.com>
2017-03-30 15:29:48 +0800

    TumblrMenu

--- TumblrMenu/TumblrMenu/MenuViewController.swift
+++ TumblrMenu/TumblrMenu/MenuViewController.swift
@@ -10,26 +10,66 @@ import UIKit
 
 class MenuViewController: UIViewController {
 
+    var btns = [UIButton]()
+    let transitionManager = MenuTransitionManger()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        setupView()
     }
     
+    private func setupView() {
+        transitioningDelegate = transitionManager
+        view.backgroundColor = UIColor(white: 1, alpha: 0.5)
+        let width: CGFloat = SWidth/2
+        let height: CGFloat = SHeight/3
+        let names = ["Audio","Chat","Link","Photo","Quote","Text"]
+        for index in 0...5 {
+            let rect = CGRect(x: width*CGFloat(index%2), y: height*CGFloat(index/2), width: width, height: height)
+            let btn = UIButton(frame: rect)
+            btn.setTitle(names[index], for: .normal)
+            btn.setTitleColor(.white, for: .normal)
+            btn.setImage(UIImage(named: names[index]), for: .normal)
+            view.addSubview(btn)
+            btn.alignContentVerticallyByCenter()
+            btn.addTarget(self, action: #selector(dismissView), for: .touchUpInside)
+            btns.append(btn)
+        }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
+    
+    func dismissView() {
+        dismiss(animated: true, completion: nil)
+    }
 
+}
+
+extension UIButton {
+    //设置按钮图片在上，文字在下的效果
+    func alignContentVerticallyByCenter() {
+        contentHorizontalAlignment = .center
+        contentVerticalAlignment = .center
+        
+        //图片与title之间有一个默认的间隔10
+        let offset: CGFloat = 10
+        
+        //在有的iOS版本上，会出现获得不到frame的情况，加上下面这两句可以100%得到
+        titleLabel?.backgroundColor = backgroundColor
+        imageView?.backgroundColor = backgroundColor
+        
+        //title
+        let titleWidth = titleLabel?.frame.size.width
+        let titleHeight = titleLabel?.frame.size.height
+        let titleLef = titleLabel?.frame.origin.x
+        let titleRig = frame.size.width-titleLef!-titleWidth!
+        
+        //image
+        let imageWidth = imageView?.frame.size.width
+        let imageHeight = imageView?.frame.size.height
+        let imageLef = imageView?.frame.origin.x
+        let imageRig = frame.size.width-imageLef!-imageWidth!
+        
+        imageEdgeInsets = UIEdgeInsets(top: 0, left: -imageLef!, bottom: titleHeight!, right: -imageRig)
+        titleEdgeInsets = UIEdgeInsets(top: imageHeight!+offset, left: -titleLef!, bottom: 0, right: -titleRig)
+    }
 }
--- TumblrMenu/TumblrMenu/ViewController.swift
+++ TumblrMenu/TumblrMenu/ViewController.swift
@@ -12,14 +12,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func setupView() {
+        let imageView = UIImageView(frame: SRect)
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = #imageLiteral(resourceName: "bg")
+        view.addSubview(imageView)
+        
+        let visualView = UIVisualEffectView(effect: UIBlurEffect(style: .light))
+        visualView.frame = SRect
+        view.addSubview(visualView)
     }
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        present(MenuViewController(), animated: true, completion: nil)
+    }
 
 }
 
