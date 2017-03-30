e43d6231c0cd949c8fc103d9cebd52c9a75d2f67
Silence <374619540@qq.com>
2017-03-30 15:29:35 +0800

    TumblrMenu

---
+++ TumblrMenu/TumblrMenu/MenuTransitionManger.swift
@@ -0,0 +1,13 @@
+//
+//  MenuTransitionManger.swift
+//  TumblrMenu
+//
+//  Created by 杨晴贺 on 2017/3/30.
+//  Copyright © 2017年 Silence. All rights reserved.
+//
+
+import UIKit
+
+class MenuTransitionManger: NSObject {
+
+}
