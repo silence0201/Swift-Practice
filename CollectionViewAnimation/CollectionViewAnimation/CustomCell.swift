cc8e000c51cceeed47f712faca72030ad9b40b4e
Silence <374619540@qq.com>
2017-03-30 16:58:23 +0800

    CollectionViewAnimation

--- CollectionViewAnimation/CollectionViewAnimation/CustomCell.swift
+++ CollectionViewAnimation/CollectionViewAnimation/CustomCell.swift
@@ -8,6 +8,52 @@
 
 import UIKit
 
+let imageRect = CGRect(x: 0, y: 0, width: ItemWidth, height: ItemHeight)
+let textRect = CGRect(x: 0, y: ItemHeight-40, width: ItemWidth, height: 40)
+
 class CustomCell: UICollectionViewCell {
+    let imageV = UIImageView(frame: imageRect)
+    let textV = UITextView(frame: textRect)
+    let backBtn = UIButton(frame: CGRect(x: 10,y:10,width:40,height: 40))
     
+    // 定义一个无参数，无返回值的闭包属性，用于处理button被点击的情况
+    typealias TapBlock = (() -> ())?
+    var backButtonTapped: TapBlock
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        imageV.contentMode = .scaleAspectFit
+        imageV.clipsToBounds = true
+        textV.font = UIFont.systemFont(ofSize: 14)
+        textV.isUserInteractionEnabled = false
+        backBtn.setImage(UIImage(named: "Back-icon"), for: .normal)
+        backBtn.isHidden = true
+        backBtn.addTarget(self, action: #selector(backBtnTouch), for: .touchUpInside)
+        backgroundColor = UIColor.gray
+
+        addSubview(imageV)
+        addSubview(textV)
+        addSubview(backBtn)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func handleCellSelected() {
+        backBtn.isHidden = false
+        superview?.bringSubview(toFront: self)
+    }
+    
+    @objc private func backBtnTouch() {
+        backBtn.isHidden = true
+        backButtonTapped!()
+    }
+    
+    func prepareCell(model: CellModel) {
+        imageV.frame = imageRect
+        textV.frame = textRect
+        imageV.image = UIImage(named: model.imageName)
+        textV.text = model.title
+    }
 }
--- CollectionViewAnimation/CollectionViewAnimation/ViewController.swift
+++ CollectionViewAnimation/CollectionViewAnimation/ViewController.swift
@@ -8,18 +8,105 @@
 
 import UIKit
 
+let ItemHeight = SHeight/3-20
+let ItemWidth = SWidth-20
+
+//数据结构体
+struct CellModel {
+    let imageName: String
+    let title: String
+    
+    init(imageName: String?, title: String?) {
+        self.imageName = imageName ?? ""
+        self.title = title ?? "没有title"
+    }
+    
+    static func getData() -> [CellModel]? {
+        let txt = "Hedge fund billionaire Bill Ackman was humbled in 2015. His Pershing Square Capital Management had a terrible year, posting a -19.3% gross return."
+        let iamgeNames = ["1","2","3","4","5"]
+        let titles = Array(repeating: txt, count: 5)
+        var result = [CellModel]()
+        for (index,name) in iamgeNames.enumerated() {
+            result.append(CellModel(imageName: name, title: titles[index]))
+        }
+        return result
+    }
+}
+
+
 class ViewController: UIViewController {
 
+    var collectionView: UICollectionView!
+    var datas = CellModel.getData()
+    
+    let reuseIdentifier = String(describing: CustomCell.self)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupView()
+    }
+    
+    func setupView() {
+        let collectionLayout = UICollectionViewFlowLayout()
+        collectionLayout.scrollDirection = .vertical//滚动方向
+        collectionLayout.itemSize = CGSize(width: ItemWidth, height: ItemHeight)//cell大小
+        collectionLayout.minimumLineSpacing = 10//上下间隔
+        collectionLayout.minimumInteritemSpacing = 10//左右间隔
+        collectionLayout.sectionInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)//section边界
+        collectionView = UICollectionView(frame: SRect, collectionViewLayout: collectionLayout)
+        collectionView.backgroundColor = .orange
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(CustomCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        view.addSubview(collectionView)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+}
+
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
     }
-
-
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return (datas?.count)!
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CustomCell
+        cell.prepareCell(model: (datas?[indexPath.row])!)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        if !collectionView.isScrollEnabled {
+            return
+        }
+        
+        guard let cell = collectionView.cellForItem(at: indexPath) as? CustomCell else {
+            return
+        }
+        collectionView.isScrollEnabled = false
+        cell.handleCellSelected()
+        cell.backButtonTapped = {
+            print("闭包执行")
+            collectionView.isScrollEnabled = true
+            collectionView.reloadItems(at: [indexPath])
+        }
+        UIView.animate(withDuration: 1,
+                       delay: 0,
+                       usingSpringWithDamping: 1,
+                       initialSpringVelocity: 1,
+                       options: [],
+                       animations: {
+                        cell.frame = CGRect(x: 10, y: self.collectionView.contentOffset.y, width: SWidth-20, height: SHeight)
+                        cell.imageV.frame = cell.bounds
+                        cell.textV.frame = CGRect(x: 0, y: SHeight-60, width: SWidth-20, height: 60)
+        }) { (finish) in
+            print("动画结束")
+        }
+    }
 }
 
