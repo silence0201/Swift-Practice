7567e83534f8dd8e94f9bd24d4f5cbf477c3df2a
Silence <374619540@qq.com>
2017-03-30 10:57:47 +0800

    ShapeLayerAnimation

--- ShapeLayerAnimation/ShapeLayerAnimation/ShapeLayerAnimation.swift
+++ ShapeLayerAnimation/ShapeLayerAnimation/ShapeLayerAnimation.swift
@@ -10,26 +10,67 @@ import UIKit
 
 class ShapeLayerAnimation: UIViewController {
 
+    let centerX: Double = 200.0
+    let centerY: Double = 300.0
+    
+    fileprivate let shapeLayer = CAShapeLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        title = "ShapeLayer动画"
+        setupView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private func setupView() {
+        shapeLayer.fillColor = UIColor.clear.cgColor // 填充颜色
+        shapeLayer.lineWidth = 20.0 // 线条宽度
+        shapeLayer.lineCap = "round" // 线条风格
+        shapeLayer.lineJoin = "round" // 链接风格
+        shapeLayer.strokeColor = UIColor.red.cgColor // 线条颜色
+        view.layer.addSublayer(shapeLayer)
+        
+        // 贝塞尔曲线
+        let path = UIBezierPath(arcCenter: CGPoint(x: centerX, y: centerY), radius: 80, startAngle: 0, endAngle: CGFloat(Double.pi*2), clockwise: false)
+        // 关联layer和贝塞尔曲线
+        shapeLayer.path = path.cgPath
+        // 创建动画，strokeEnd属性
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.fromValue = 0.0
+        animation.toValue = 1.0
+        shapeLayer.autoreverses = false
+        animation.duration = 3.0
+        
+        // 设置动画
+        shapeLayer.add(animation, forKey: nil)
+        animation.delegate = self
+        
+        //外围16根线条
+        let count = 16
+        for i in 0..<count {
+            let line = CAShapeLayer()
+            line.fillColor = UIColor.clear.cgColor
+            line.strokeColor = UIColor.yellow.cgColor
+            line.lineWidth = 15.0
+            line.lineCap = "round"
+            line.lineJoin = "round"
+            view.layer.addSublayer(line)
+            
+            let path2 = UIBezierPath()
+            let x = centerX+100*cos(2.0*Double(i)*Double.pi/Double(count))
+            let y = centerY-100*sin(2.0*Double(i)*Double.pi/Double(count))
+            let len = 50
+            path2.move(to: CGPoint(x: x, y: y))
+            path2.addLine(to: CGPoint(x: x+Double(len)*cos(2*Double.pi/Double(count)*Double(i)), y: y-Double(len)*sin(2*Double.pi/Double(count)*Double(i))))
+            line.path = path2.cgPath
+            line.add(animation, forKey: nil)
+        }
     }
-    */
 
+}
+
+extension ShapeLayerAnimation: CAAnimationDelegate {
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        //结束后设置整个填充为红色
+        shapeLayer.fillColor = UIColor.red.cgColor
+    }
 }
--- ShapeLayerAnimation/ShapeLayerAnimation/ViewController.swift
+++ ShapeLayerAnimation/ShapeLayerAnimation/ViewController.swift
@@ -10,14 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let btn1 = UIButton(frame: CGRect(x: 100, y: 200, width: SWidth-200, height: 30))
+    let btn2 = UIButton(frame: CGRect(x: 100, y: 300, width: SWidth-200, height: 30))
+    
+    var drawView = DrawRectView(frame: CGRect(x: 0, y: 64, width: SWidth, height: SHeight-64))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupView() {
+        title = "画图与动画"
+        view.backgroundColor = .white
+        
+        btn1.setTitle("DrawRect", for: .normal)
+        btn1.setTitleColor(.orange, for: .normal)
+        btn2.setTitle("CAShapeLayer", for: .normal)
+        btn2.setTitleColor(.orange, for: .normal)
+        
+        btn1.addTarget(self, action: #selector(showView), for: .touchUpInside)
+        btn2.addTarget(self, action: #selector(showView), for: .touchUpInside)
+        
+        view.addSubview(btn1)
+        view.addSubview(btn2)
+    }
+    
+    func showView(_ sender: UIButton) {
+        if sender.currentTitle == "DrawRect" {
+            print("采用drawrect画图")
+            view.addSubview(drawView)
+        }else {
+            print("采用cashapelayer")
+            navigationController?.pushViewController(ShapeLayerAnimation(), animated: true)
+        }
     }
 
 
