73d82c17b515ca639d52683d773e5d2dec98b9dd
Silence <374619540@qq.com>
2017-03-30 10:57:35 +0800

    ShapeLayerAnimation

---
+++ ShapeLayerAnimation/ShapeLayerAnimation/DrawRectView.swift
@@ -0,0 +1,21 @@
+//
+//  DrawRectView.swift
+//  ShapeLayerAnimation
+//
+//  Created by 杨晴贺 on 2017/3/30.
+//  Copyright © 2017年 Silence. All rights reserved.
+//
+
+import UIKit
+
+class DrawRectView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
