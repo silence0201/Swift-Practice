9629909db216b47e2bcf6b923e8cc0ffef9a29fc
Silence <374619540@qq.com>
2017-03-30 14:18:27 +0800

    CustomPushTransition

---
+++ CustomPushTransition/CustomPushTransition/PushAnimation.swift
@@ -0,0 +1,13 @@
+//
+//  PushAnimation.swift
+//  CustomPushTransition
+//
+//  Created by 杨晴贺 on 2017/3/30.
+//  Copyright © 2017年 Silence. All rights reserved.
+//
+
+import UIKit
+
+class PushAnimation: NSObject {
+
+}
