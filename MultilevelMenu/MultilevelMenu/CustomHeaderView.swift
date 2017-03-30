5a0e186424d0329445f0deef25ea0fc4dd518c53
Silence <374619540@qq.com>
2017-03-30 22:25:14 +0800

    MultilevelMenu

---
+++ MultilevelMenu/MultilevelMenu/CustomHeaderView.swift
@@ -0,0 +1,48 @@
+//
+//  CustomHeaderView.swift
+//  MultilevelMenu
+//
+//  Created by 杨晴贺 on 2017/3/30.
+//  Copyright © 2017年 Silence. All rights reserved.
+//
+
+import UIKit
+
+class CustomHeaderView: UITableViewHeaderFooterView {
+
+    let imageView = UIImageView()
+    let title = UILabel()
+    let detail = UILabel()
+    
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        imageView.contentMode = .center
+        title.font = UIFont.systemFont(ofSize: 20)
+        title.textAlignment = .left
+        addSubview(imageView)
+        addSubview(title)
+        addSubview(detail)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.frame = CGRect(x: 0, y: 0, width: frame.height, height: frame.height)
+        title.frame = CGRect(x: frame.height, y: 0, width: frame.width-2*frame.height, height: frame.height)
+        detail.frame = CGRect(x: frame.width-frame.height, y: 0, width: frame.height, height: frame.height)
+    }
+    
+    
+    func buildUI(province: Province) {
+        imageView.image = UIImage(named: province.isOpen ? "Down" : "Right")
+        title.text = province.name
+        detail.text = String(province.citys.count)+"个市"
+    }
+
+
+}
