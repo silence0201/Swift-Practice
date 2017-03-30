e65ac1d19b1fae8b3ad6992f0b9df743e3c1d6e3
Silence <374619540@qq.com>
2017-03-30 15:47:58 +0800

    LoginView

---
+++ LoginView/LoginView/Extension.swift
@@ -0,0 +1,13 @@
+//
+//  Extension.swift
+//  LoginView
+//
+//  Created by 杨晴贺 on 2017/3/30.
+//  Copyright © 2017年 Silence. All rights reserved.
+//
+
+import UIKit
+
+class Extension: NSObject {
+
+}
